cb1b027f38f329f260ec2c7578ad24956b5fa5d6
ronakmajumder1 <ronakmajumder1@gmail.com>
2026-03-20 15:00:49 +0530

    Implement palindrome checking function in LC_125

--- src/Recursion/PalindromString.cpp
+++ src/Recursion/PalindromString.cpp
@@ -9,8 +9,29 @@ bool f(int i, string s){
     return f(i+1, s);        
 }
 
+bool LC_125(string s){
+    string newString = "";
+        
+        for(char c : s){
+            if(isalnum(c)) newString += tolower(c);
+        }
+
+        int len = newString.size();
+        int start = 0, end = len - 1;
+        while(start < len/2){
+            if(newString[start] != newString[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+}
+
 int main(){
     string s = "iababar";
-    cout << endl << f(0, s);
+    string str = "A man, a plan, a canal: Panama";
+    cout << endl << LC_125(str);
+    //cout << endl << f(0, s);
     return 0;
 }
